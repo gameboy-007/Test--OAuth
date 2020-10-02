18c65884f45b163d879299eaf1becb1fc96b21f9
DEEPESH001 <32238571+DEEPESH001@users.noreply.github.com>
2020-10-02 18:52:48 +0530

    files uploaded

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '623253116456-8upgor09efkdssv7espm0tcjv8m6do0a.apps.googleusercontent.com',
+            clientSecret: 'zXA-Pume8wCZMQTziNji2QMt'
+        },
+        mongodb: {
+            dbURI: 'mongodb+srv://test:test1@cluster0.q7wul.mongodb.net/test'
+        },
+        session: {
+            cookieKey: 'thisisthegodkey'
+        }
+};
